59b4b162d47f91f5d63fec09e47ba60f702ad726
Nicolette <molitorn@my.lanecc.edu>
2018-09-30 11:24:21 -0700

    finished boneyard class and tests

--- MTD/MTDClasses/BoneYard.cs
+++ MTD/MTDClasses/BoneYard.cs
@@ -4,38 +4,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MTDClasses
 {
-    class BoneYard
+    public class BoneYard
     {
-        /*
-        public BoneYard(int maxDots)
-        {
+       private List<Domino> listOfDominoes = new List<Domino>(); 
+
+        public BoneYard(int maxDots) //populates the list of all possible combinations from double blank to double maxDots
+        { 
+                      
+            for ( int i = 0; i<= maxDots; i++)
+            {
+              
+                for (int j = 0; j<= maxDots; j++)
+                {
+                    Domino domino = new Domino(i, j);
+                    listOfDominoes.Add(domino);
+                }
+            }
+            Shuffle(); //Shuffles each time a new instance is started
         }
 
-        public void Shuffle()
+        public void Shuffle() //Shuffles the boneYard list
         {
+            List<Domino> randomList = new List<Domino>();
+            Random r = new Random();
+            int randomIndex = 0;
+            while (listOfDominoes.Count > 0)
+            {
+                randomIndex = r.Next(0, listOfDominoes.Count);
+                randomList.Add(listOfDominoes[randomIndex]);
+                listOfDominoes.RemoveAt(randomIndex);
+            }
+            listOfDominoes = randomList;
         }
 
-        public bool IsEmpty()
+
+        public bool IsEmpty() //Checks to see if the list is empty
         {
+            if (DominosRemaining == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        public int DominosRemaining
+        public int DominosRemaining // Counts all the dominoes in the boneyard at any point
         {
+            get
+            {
+                return listOfDominoes.Count;
+            }
         }
 
-        public Domino Draw()
+        public Domino Draw() //Draws a Domino from the boneYard.
         {
+            
+            //write random number generator between 0 and dominoes remaining -1
+            Random rnd = new Random();
+            int index = rnd.Next(DominosRemaining);
+            
+            Domino domino;
+            domino = this[index];
+            listOfDominoes.Remove(domino);
+            return domino;
         }
 
-        public Domino this[int index]
+        public Domino this[int index] // gets and sets the domino in the list at a specific index
         {
+            get
+            {
+               
+                return listOfDominoes[index];
+            }
+            set
+            {
+                 listOfDominoes[index] = value;
+
+            }
         }
 
-        public override string ToString()
+        public override string ToString() //Return a string list of what is in the boneyard
         {
+            if (this.IsEmpty())
+            {
+                return ("The boneYard is empty!");
+            }
+            else 
+            {
+                string output = "";
+                foreach (Domino d in listOfDominoes)
+                {
+                    output += d.ToString() + "\n";
+                }
+                return output;
+            }
         }
-        */
+
     }
 }
---
+++ MTD/MTDTests/BoneYardTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using MTDClasses;
+
+namespace MTDTests
+{
+
+    [TestFixture]
+    public class BoneYardTests
+    {
+        BoneYard boneYard;
+        BoneYard boneYard2;
+        BoneYard boneYard3;
+
+
+        [SetUp]
+        public void SetUpAllTests()
+        {
+            boneYard = new BoneYard(2);
+            boneYard2 = new BoneYard(2);
+            boneYard3 = new BoneYard(12);
+
+        }
+
+        [Test]
+        public void TestBoneYardConstructor() 
+        {
+            //int count = boneYard.DominosRemaining;
+            //Assert.AreEqual(9, count);
+
+            //int expected = 9;
+            //int actual = boneYard.DominosRemaining;
+            //Assert.AreEqual(expected, actual);
+
+            Assert.IsInstanceOf<BoneYard>(boneYard);
+
+        }
+
+        [Test]
+        public void TestBoneYardIsEmpty()
+
+        {
+            bool expected = false;
+            bool actual = boneYard.IsEmpty();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestBoneYardDraw()
+        {
+           
+            Domino domino = boneYard.Draw();
+            Assert.IsInstanceOf<Domino>(domino);
+        }
+        
+        [Test]
+        public void TestBoneYardDominosRemaining()
+        {
+            int expected = 8;
+            boneYard2.Draw();
+            int actual = boneYard2.DominosRemaining;
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void TestBoneYardThisGetter()
+        {
+            Domino domino = boneYard[0];
+            Assert.IsInstanceOf<Domino>(domino);
+        }
+
+        [Test]
+        public void TestBoneYardThisSetter()
+        {
+            int expected = 12;
+            boneYard[1].Side1 = 12;
+            int actual = boneYard[1].Side1;
+            Assert.AreEqual(expected, actual);
+
+        }
+        [Test]
+        public void TestBoneYardToString()
+        {
+            String boneYardStringList;
+            boneYardStringList = boneYard.ToString();
+            Assert.IsInstanceOf<String>(boneYardStringList);
+
+        }
+
+        [Test]
+        public void TestBoneYardShuffle() //1 in 169 tests will fail
+        {
+            Domino expected = boneYard3[0];
+            boneYard3.Shuffle();
+            Domino actual = boneYard3[0];
+            Assert.AreNotEqual(expected, actual);
+
+        }
+    }
+}
